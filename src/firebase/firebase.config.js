1a6aae958d7ed5461029c0e5d6039e08b2e038ec
Abdur <rahmanabdurr65@gmail.com>
2023-05-29 11:16:38 -1100

    update build for gh-pages

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCMhbOx2_ElMl6InYoQRdFVcjwYU_L8Ahk",
+    authDomain: "emajohn-simple-d0273.firebaseapp.com",
+    projectId: "emajohn-simple-d0273",
+    storageBucket: "emajohn-simple-d0273.appspot.com",
+    messagingSenderId: "1056120864669",
+    appId: "1:1056120864669:web:82d7c248b745a265f33974"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
